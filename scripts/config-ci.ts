593adf926cfc0cbe3e4691d9e9d15d68e5d996a9
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-11-14 09:35:54 -0800

    update script

--- scripts/config-ci.ts
+++ scripts/config-ci.ts
@@ -24,19 +24,20 @@ if (!args["--force"]) {
   if (!files.length) process.exit();
 }
 
-console.log("Generating new bitbucket-pipeline.yml...");
+console.log("Updating bitbucket-pipeline.yml...");
 
 const { stdout: info } = $.sync`yarn turbo run //#config-ci --dry-run=json`;
 
 const FILENAME = "bitbucket-pipelines.yml";
 const globalHash =
   JSON.parse(info).globalCacheInputs.hashOfExternalDependencies;
-const doc = parseDocument(
-  readFileSync(resolve(`templates/${FILENAME}`), "utf-8"),
-);
+const doc = parseDocument(readFileSync(resolve(FILENAME), "utf-8"));
+const HASH_REGEX = /[a-f0-9]{16}$/;
 
-function stamp(key: Scalar<string>) {
-  return `${key.value}-${globalHash}`;
+function pin(key: Scalar<string>) {
+  return HASH_REGEX.test(key.value)
+    ? key.value.replace(HASH_REGEX, globalHash)
+    : `${key.value}-${globalHash}`;
 }
 
 const target = "caches";
@@ -49,14 +50,14 @@ visit(doc, {
         const caches = new YAMLMap();
         value.items.forEach(({ key, value }) => {
           caches.add(
-            new Pair(stamp(key as Scalar<string>), (value as YAMLMap).clone()),
+            new Pair(pin(key as Scalar<string>), (value as YAMLMap).clone()),
           );
         });
         return new Pair(target, caches);
       } else if (isSeq(value)) {
         const caches = new YAMLSeq();
         value.items.forEach((item) => {
-          caches.add(stamp(item as Scalar<string>));
+          caches.add(pin(item as Scalar<string>));
         });
         return new Pair(target, caches);
       }
