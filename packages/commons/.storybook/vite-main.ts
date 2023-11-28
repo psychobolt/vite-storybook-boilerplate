095ccedaa2b59b5dfaf6bf40bb38c7c74ce66e11
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-11-28 14:17:23 -0800

    resolve referenced addons

--- packages/commons/.storybook/vite-main.ts
+++ packages/commons/.storybook/vite-main.ts
@@ -1,6 +1,7 @@
 import { join, dirname } from "path";
-import type { StorybookConfig } from "@storybook/types";
+import type { StorybookConfig, Preset } from "@storybook/types";
 import type { StorybookConfigVite } from "@storybook/builder-vite";
+import type { AliasOptions } from "vite";
 import { defineConfig, mergeConfig } from "vite";
 import turbosnap from "vite-plugin-turbosnap";
 
@@ -12,31 +13,48 @@ export function getAbsolutePath(value: string, { resolve } = require): any {
   return dirname(resolve(join(value, "package.json")));
 }
 
-export type StorybookViteCommonConfig = StorybookConfig & StorybookConfigVite;
+const addons: Preset[] = [
+  getAbsolutePath("@storybook/addon-essentials"),
+  getAbsolutePath("@storybook/addon-onboarding"),
+  getAbsolutePath("@storybook/addon-coverage"),
+];
+const resolvePaths: AliasOptions = {};
+
+for (const addon of [
+  "@storybook/addon-links",
+  "@storybook/addon-interactions",
+]) {
+  const absolutePath = dirname(require.resolve(addon));
+  addons.push(join(absolutePath, "package.json"));
+  resolvePaths[addon] = absolutePath;
+}
 
 const mainDir = "@(src|stories)";
+
+export type StorybookViteCommonConfig = StorybookConfig & StorybookConfigVite;
 
 export const config: StorybookViteCommonConfig = {
   stories: [
     `../${mainDir}/**/*.mdx`,
     `../${mainDir}/**/*.stories.@(js|jsx|ts|tsx)`,
   ],
-  addons: [
-    require.resolve("@storybook/addon-links/manager"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-onboarding"),
-    require.resolve("@storybook/addon-interactions/manager"),
-    require.resolve("@storybook/addon-coverage/preset"),
-  ],
+  addons,
   docs: {
     autodocs: "tag",
   },
   viteFinal(config, { configType }) {
-    let finalConfig;
+    let finalConfig = mergeConfig(
+      config,
+      defineConfig({
+        resolve: {
+          alias: resolvePaths,
+        },
+      }),
+    );
 
     if (configType === "PRODUCTION") {
       finalConfig = mergeConfig(
-        config,
+        finalConfig,
         defineConfig({
           plugins: [
             // @ts-expect-error https://github.com/IanVS/vite-plugin-turbosnap/issues/8
@@ -46,7 +64,7 @@ export const config: StorybookViteCommonConfig = {
       );
     } else {
       finalConfig = mergeConfig(
-        config,
+        finalConfig,
         defineConfig({
           server: {
             fs: {
