34a08d1a93a841ec9cd82111aae17f8a398cf478
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-12-06 10:35:43 -0800

    support junit reports

--- packages/commons/.storybook/test-runner-jest.config.ts
+++ packages/commons/.storybook/test-runner-jest.config.ts
@@ -1,5 +1,8 @@
+import { createRequire } from "module";
 import type { Config } from "@jest/types";
 import { getJestConfig } from "@storybook/test-runner";
+
+const require = createRequire(import.meta.url);
 
 // The default Jest configuration comes from @storybook/test-runner
 const testRunnerConfig = getJestConfig();
@@ -11,6 +14,15 @@ const config: Config.InitialOptions = {
    * @see https://jestjs.io/docs/configuration
    */
   passWithNoTests: true,
+  reporters: [
+    "default",
+    [
+      require.resolve("jest-junit"),
+      {
+        outputDirectory: "test-reports",
+      },
+    ],
+  ],
 };
 
 export default config;
