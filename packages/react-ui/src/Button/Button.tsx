99dffdd8ebc280f18bf20aec30d0b70cf82f118b
psychobolt <560721+psychobolt@users.noreply.github.com>
2023-12-17 08:17:05 -0800

    refactor

--- packages/react-ui/src/Button/Button.tsx
+++ packages/react-ui/src/Button/Button.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import classes from "./Button.module.scss";
 
 interface ButtonProps {
--- packages/react-ui/src/Header/Header.tsx
+++ packages/react-ui/src/Header/Header.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { Button } from "../Button/index.ts";
 import classes from "./Header.module.scss";
 
