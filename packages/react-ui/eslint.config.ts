b5078c0c08190156d8982de0cba1b95bd2c4303d
Michael Tran <560721+psychobolt@users.noreply.github.com>
2024-03-26 17:03:28 -0700

    refactor (#588)

--- packages/react-ui/eslint.config.ts
+++ packages/react-ui/eslint.config.ts
@@ -1,11 +1,8 @@
-import commonConfig, {
-  compat,
-  type Config,
-} from "commons/esm/eslint.config.js";
+import jsxRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
+import defaultConfig, { type Config } from "commons/esm/eslint.config.js";
 
-const config: Config[] = [
-  ...commonConfig,
-  ...compat.extends("plugin:react/jsx-runtime"),
-];
+const commonConfig: Config = defaultConfig;
+
+const config: Config = [...commonConfig, jsxRuntime];
 
 export default config;
