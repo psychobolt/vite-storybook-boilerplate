06ea88b4bdca5bfb1a8692ebe5a98097f7899948
Michael Tran <560721+psychobolt@users.noreply.github.com>
2024-04-01 17:12:50 -0700

    Migrate workspaces to pnpm linker. Closes #483 (#609)
    * migrate to pnpm
    
    * test
    
    * fix
    
    * add workaround
    
    * ignore empty stash
    
    * keep turbo version consistent with workspace
    
    * print version
    
    * only query from root
    
    * refactor
    
    * refactor

--- packages/html-ui/stylelint.config.js
+++ packages/html-ui/stylelint.config.js
@@ -1,14 +1,6 @@
-import { createRequire } from 'module';
-import path from 'path';
-
-const require = createRequire(
-  process.env.INIT_CWD ??
-    path.join(process.cwd(), 'packages/third-party/node_modules')
-);
-
 /** @type {import('stylelint').Config} */
 export default {
-  extends: [require.resolve('commons/esm/stylelint.config')],
+  extends: ['../commons/esm/stylelint.config'],
   rules: {
     'selector-class-pattern': [
       '^([a-z][a-z0-9]*)(--?[a-z0-9]+)*$',
