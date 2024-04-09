5261f07bd941a95e9b9edd25ca561c056dec3cb0
psychobolt <560721+psychobolt@users.noreply.github.com>
2024-04-09 10:17:49 -0700

    refactor

--- bin/bootstrap.ts
+++ bin/bootstrap.ts
@@ -25,7 +25,7 @@ async function* getWorkspacesByLinker() {
 
 const getGlobalFolder = () => $.sync`yarn config get globalFolder`.stdout;
 const setGlobalFolder = (path: string) =>
-  $.sync`yarn config set globalFolder "${path}"`;
+  $.sync`yarn config set globalFolder ${path}`;
 
 for await (const [linker, workspaces] of getWorkspacesByLinker()) {
   if (!workspaces.length) continue;
@@ -33,17 +33,19 @@ for await (const [linker, workspaces] of getWorkspacesByLinker()) {
   console.log(`Verify workspaces using ${linker} linker...`);
 
   if (linker === 'pnpm') {
+    console.log('Verifying global index linking...');
     const globalFolder = getGlobalFolder();
-    const testFile = 'dummy.txt';
-    const testFilePath = join(globalFolder, testFile);
+    const indexFile = 'dummy.dat';
+    const indexPath = join(globalFolder, indexFile);
+    const indexURL = new URL(indexPath, 'file://');
     const root = join(import.meta.dirname, '..');
     const temp = join(root, 'temp');
-    const tempFile = join(temp, testFile);
+    const link = join(temp, indexFile);
     if (!fs.existsSync(temp)) fs.mkdirSync(temp);
-    fs.writeFileSync(testFilePath, tempFile);
+    fs.writeFileSync(indexPath, '');
     try {
-      fs.symlinkSync(testFilePath, tempFile);
-      fs.unlinkSync(tempFile);
+      fs.symlinkSync(indexURL.href, link);
+      fs.unlinkSync(link);
     } catch (e) {
       console.log(
         'Failed to link to global folder. Attempting to migrate to local folder...'
@@ -53,8 +55,9 @@ for await (const [linker, workspaces] of getWorkspacesByLinker()) {
       console.log('Copying cache files to local folder...');
       fs.cpSync(globalFolder, localFolder, { recursive: true });
     }
-    fs.rmSync(testFilePath);
+    fs.rmSync(indexURL);
     if (!fs.readdirSync(temp).length) fs.rmSync(temp, { recursive: true });
+    console.log('Global index link was successful. Test files are cleaned up!');
   }
 
   workspaces.forEach((workspace) => {
