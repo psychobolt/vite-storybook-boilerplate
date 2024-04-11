af36fe70590df79dbc6aec6a9bf59ec020323ad2
psychobolt <560721+psychobolt@users.noreply.github.com>
2024-04-10 22:11:53 -0700

    refactor

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,1 +1,7 @@
-export { default } from 'commons/esm/lint-staged.config.js';
+import defaultConfig from 'commons/esm/lint-staged.config.js';
+
+/** @type {import('lint-staged').Config} */
+export default {
+  './bitbucket-pipelines.yml': () => 'yarn exec turbo run //#config-ci',
+  '!(bitbucket-pipelines.yml)': defaultConfig['*']
+};
