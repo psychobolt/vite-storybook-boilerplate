7332a1b40ba0f970d259a39722e0e2606743fe9f
psychobolt <560721+psychobolt@users.noreply.github.com>
2024-04-15 23:09:57 -0700

    refactor

---
+++ renovate.config.cjs
@@ -0,0 +1,27 @@
+const { execSync } = require('node:child_process');
+const { join } = require('node:path');
+
+const stdout = execSync(
+  'yarn ls-workspaces --node-linker=pnpm --node-linker=node-modules'
+);
+
+const workspaces = JSON.parse(stdout);
+
+module.exports = {
+  platform: 'bitbucket',
+  repositories: ['psychobolt/vite-storybook-boilerplate'],
+  allowedPostUpgradeCommands: ['^.+$'],
+  packageRules: [
+    {
+      matchFileNames: workspaces.map(({ location }) =>
+        join(location, 'package.json')
+      ),
+      postUpgradeTasks: {
+        commands: [
+          'YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install && yarn bootstrap'
+        ],
+        fileFilters: ['**/yarn.lock', 'bitbucket-pipelines.yml']
+      }
+    }
+  ]
+};
