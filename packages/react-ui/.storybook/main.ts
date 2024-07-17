3c85f29d0409fffb8539dab547b6be2bf44fda91
psychobolt <560721+psychobolt@users.noreply.github.com>
2024-07-17 15:54:14 -0700

    refactor

--- packages/react-ui/.storybook/main.ts
+++ packages/react-ui/.storybook/main.ts
@@ -1,10 +1,13 @@
 import { createRequire } from 'node:module';
 import type { StorybookConfig } from '@storybook/react-vite';
+import { mergeConfig } from 'vite';
 import type { StorybookViteCommonConfig } from 'commons/esm/.storybook/vite-main';
 import {
   config as commonConfig,
   getAbsolutePath
 } from 'commons/esm/.storybook/vite-main';
+
+import viteConfig from './vite.config.ts';
 
 const require = createRequire(import.meta.url);
 
@@ -13,7 +16,9 @@ export const config: StorybookConfig | StorybookViteCommonConfig = {
   framework: {
     name: getAbsolutePath('@storybook/react-vite', require),
     options: {}
-  }
+  },
+  viteFinal: (config, options) =>
+    mergeConfig(commonConfig.viteFinal(config, options), viteConfig)
 };
 
 export default config;
