ce362f29fd95c2c1c23ba884b44cedfafa54453c
psychobolt <560721+psychobolt@users.noreply.github.com>
2024-10-21 15:38:36 -0700

    fix

--- packages/html-ui/modules.d.ts
+++ packages/html-ui/modules.d.ts
@@ -1,18 +1,17 @@
-import type { WebComponentsRenderer } from '@storybook/web-components';
-
 declare module 'commons/esm/.storybook/addons/addon-variants' {
-  import type { VariantsMeta as DefaultVariantsMeta } from 'commons/.storybook/addons/addon-variants';
+  import type { WebComponentsRenderer } from '@storybook/web-components';
+  import type { VariantsMeta as DefaultVariantsMeta } from 'commons/esm/.storybook/addons/addon-variants.d.ts';
 
-  export * from 'commons/.storybook/addons/addon-variants';
+  export * from 'commons/esm/.storybook/addons/addon-variants.d.ts';
 
   export interface VariantsMeta<TArgs>
     extends DefaultVariantsMeta<TArgs, WebComponentsRenderer> {}
 }
 
 declare module 'commons/esm/.storybook/utils/story-generators' {
-  import type { StoryPseudoStateProps as DefaultStoryPseudoStateProps } from 'commons/.storybook/utils/story-generators';
+  import type { StoryPseudoStateProps as DefaultStoryPseudoStateProps } from 'commons/esm/.storybook/utils/story-generators.d.ts';
 
-  export * from 'commons/.storybook/utils/story-generators';
+  export * from 'commons/esm/.storybook/utils/story-generators.d.ts';
 
   export interface DirectiveResult {
     values: Array<{ [key: string]: string }>;
