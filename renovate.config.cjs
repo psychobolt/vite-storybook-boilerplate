7bb22a993e7d1b5fb246cf8171c9afc1ed2362a6
psychobolt <560721+psychobolt@users.noreply.github.com>
2025-03-12 16:06:15 -0700

    refactor

--- renovate.config.cjs
+++ renovate.config.cjs
@@ -5,7 +5,6 @@ const workspaces = process.env.RENOVATE_POST_UPGRADE_WORKSPACES
   : [];
 
 module.exports = {
-  dependencyDashboard: process.env.RENOVATE_DASHBOARD !== 'false',
   allowedCommands: ['^.+$'],
   packageRules: [
     {
