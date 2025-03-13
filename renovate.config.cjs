f5bc52ec5e577e017ae0a28527cba98bdb30cbb8
psychobolt <560721+psychobolt@users.noreply.github.com>
2025-03-12 18:03:22 -0700

    refactor

--- renovate.config.cjs
+++ renovate.config.cjs
@@ -8,7 +8,6 @@ const workspaces = process.env.RENOVATE_POST_UPGRADE_WORKSPACES
 module.exports = {
   extends: ['config:best-practices'],
   automerge: true,
-  dependencyDashboard: true,
   nvm: {
     enabled: false
   },
