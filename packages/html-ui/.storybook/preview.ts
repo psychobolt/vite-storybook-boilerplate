5f15a4dd8a3f28229092efe40821d1082f5be80a
psychobolt <560721+psychobolt@users.noreply.github.com>
2025-05-30 14:21:08 -0700

    fix

--- packages/html-ui/.storybook/preview.ts
+++ packages/html-ui/.storybook/preview.ts
@@ -1,4 +1,4 @@
-import '@types/react-syntax-highlighter';
+import 'react-syntax-highlighter';
 
 import type { Preview } from '@storybook/web-components';
 import commonConfig from 'commons/esm/.storybook/preview';
