e8937c08b796c56895a275e4d1df372b77227e10
psychobolt <560721+psychobolt@users.noreply.github.com>
2025-10-05 14:29:31 -0700

    fix

--- apps/svelte-app/src/routes/Header.svelte
+++ apps/svelte-app/src/routes/Header.svelte
@@ -1,5 +1,6 @@
 <script>
-	import { page } from '$app/stores';
+	import { resolve } from '$app/paths';
+	import { page } from '$app/state';
 	import logo from '$lib/images/svelte-logo.svg';
 	import github from '$lib/images/github.svg';
 </script>
@@ -16,14 +17,14 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
+			<li aria-current={page.url.pathname === '/' ? 'page' : undefined}>
+				<a href={resolve('/', {})}>Home</a>
 			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
+			<li aria-current={page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href={resolve('/about', {})}>About</a>
 			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
+			<li aria-current={page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href={resolve('/sverdle', {})}>Sverdle</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
