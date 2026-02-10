499894607026bec863e76bb33926ac90aff360a0
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-02-10 09:01:50 -0800

    refactor

--- packages/react-ui/stories/Docs/Add-Ons/Blocks.story.ts
+++ packages/react-ui/stories/Docs/Add-Ons/Blocks.story.ts
@@ -1,18 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 
-import { type Props, Badge } from './Badge';
+import { type Props, Markdown } from './Markdown';
 
 const meta = {
-  title: 'Components/Badge',
+  title: 'Add Ons/Blocks/Markdown',
   tags: ['autodocs'],
-  component: Badge
+  component: Markdown
 } satisfies Meta<Props>;
 
 export default meta;
 
 type Story = StoryObj<Props>;
 
-export const Static: Story = {
+export const Badge: Story = {
   args: {
     children:
       '[![MIT License](https://img.shields.io/badge/license-MIT-blue)](https://img.shields.io/badge/license-MIT-blue)'
