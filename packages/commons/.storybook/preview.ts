08dbfa9d3d8aca6d205265dd1ed9a8e66772a03c
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-02-11 23:10:49 -0800

    wip

--- packages/commons/.storybook/preview.ts
+++ packages/commons/.storybook/preview.ts
@@ -2,43 +2,37 @@ import addonDocs from '@storybook/addon-docs';
 import {
   type Renderer,
   type ProjectAnnotations,
-  type PreviewAddon,
   type InferTypes,
-  definePreview as _definePreview
+  type PreviewAddon,
+  type Preview,
+  definePreview
 } from 'storybook/internal/csf';
-import _ from 'lodash';
 
-type InferCSF4<TRenderer> = TRenderer extends Renderer
-  ? TRenderer & { csf4: true }
-  : never;
+const addons = [addonDocs()];
 
-export const definePreview = <
-  TRenderer extends Renderer,
-  TAddons extends PreviewAddon<never>[],
-  TPreview extends typeof _definePreview<InferCSF4<TRenderer>, TAddons> =
-    typeof _definePreview<InferCSF4<TRenderer>, TAddons>
->(
-  ...[input]: Parameters<TPreview>
-) => {
-  const addons = [addonDocs()];
-  const projectAddons = [...addons, ...(input.addons ?? [])];
+type Addons = typeof addons;
 
-  const defaults: ProjectAnnotations<Renderer & InferTypes<typeof addons>> = {
-    parameters: {
-      actions: { argTypesRegex: '^on[A-Z].*' },
-      docs: {},
-      controls: {
-        matchers: {
-          color: /(background|color)$/i,
-          date: /Date$/i
-        }
+type DefaultRenderer = Renderer & InferTypes<Addons>;
+
+const input: ProjectAnnotations<DefaultRenderer> & { addons: Addons } = {
+  addons,
+  parameters: {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i
       }
     }
-  };
-
-  return _definePreview<InferCSF4<TRenderer>, typeof projectAddons>(
-    _.merge(defaults, input, { addons: projectAddons })
-  );
+  }
 };
 
-export default definePreview({ addons: [] });
+export const withPreview = <
+  TRenderer extends Renderer,
+  TAddons extends PreviewAddon[],
+  TPreview extends Preview<TRenderer & DefaultRenderer & InferTypes<TAddons>>
+>(
+  preview: (defaults: typeof input) => TPreview
+) => preview(input);
+
+export default definePreview(input);
--- packages/html-ui/.storybook/preview.ts
+++ packages/html-ui/.storybook/preview.ts
@@ -1,39 +1,24 @@
 import 'react-syntax-highlighter';
-import {
-  type WebComponentsRenderer,
-  definePreview as _definePreview
-} from '@storybook/web-components-vite';
-import { definePreview } from 'commons/esm/.storybook/preview';
+
+import { definePreview } from '@storybook/web-components-vite';
 import { SyntaxHighlighter } from 'storybook/internal/components';
 import scss from 'react-syntax-highlighter/dist/esm/languages/prism/scss';
+import { withPreview } from 'commons/esm/.storybook/preview';
 
 SyntaxHighlighter.registerLanguage('scss', scss);
 
-const addons: never[] = [];
-
-const preview = definePreview<WebComponentsRenderer, typeof addons>({
-  addons,
-  parameters: {
-    options: {
-      storySort: {
-        order: ['Configure your project', 'Readme']
+export default withPreview((defaults) =>
+  definePreview({
+    ...defaults,
+    addons: [...defaults.addons],
+    parameters: {
+      ...defaults.parameters,
+      options: {
+        ...(defaults.parameters?.options as object),
+        storySort: {
+          order: ['Configure your project', 'Readme']
+        }
       }
     }
-  }
-});
-
-preview.meta({
-  parameters: {
-    layout: 'centered'
-  }
-});
-
-_definePreview({
-  addons: []
-}).meta({
-  parameters: {
-    layout: 'centered'
-  }
-});
-
-export default preview;
+  })
+);
