1648fe7c889c13d6950eadcddce4de272a9e3657
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-02-23 15:54:39 -0800

    fix

--- packages/html-ui/.storybook/preview.ts
+++ packages/html-ui/.storybook/preview.ts
@@ -1,6 +1,7 @@
 import 'react-syntax-highlighter';
 
 import {
+  type Preview,
   type WebComponentsTypes,
   definePreview
 } from '@storybook/web-components-vite';
@@ -10,35 +11,33 @@ import { withDefaults } from 'commons/esm/.storybook/preview';
 
 SyntaxHighlighter.registerLanguage('scss', scss);
 
+// See issue: https://github.com/storybookjs/storybook/issues/33798
+const parameters: Preview['parameters'] = {
+  options: {
+    storySort: {
+      order: ['Configure your project', 'Readme']
+    }
+  }
+};
+
 export default {
+  parameters,
   ...withDefaults<WebComponentsTypes>((defaults) =>
     definePreview({
       ...defaults,
       parameters: {
         ...defaults.parameters,
+        ...parameters,
         docs: {
           source: {
             async transform(code) {
               const prettier = await import('prettier/standalone');
-              const prettierPluginBabel =
-                await import('prettier/plugins/babel');
-              const prettierPluginEstree =
-                await import('prettier/plugins/estree');
               const prettierPluginHtml = await import('prettier/plugins/html');
               return prettier.format(code, {
-                parser: 'babel',
-                plugins: [
-                  prettierPluginBabel,
-                  prettierPluginEstree,
-                  prettierPluginHtml
-                ]
+                parser: 'html',
+                plugins: [prettierPluginHtml]
               });
             }
-          }
-        },
-        options: {
-          storySort: {
-            order: ['Configure your project', 'Readme']
           }
         }
       }
--- packages/react-ui/.storybook/preview.ts
+++ packages/react-ui/.storybook/preview.ts
@@ -1,18 +1,28 @@
-import { type ReactTypes, definePreview } from '@storybook/react-vite';
+import {
+  type Preview,
+  type ReactTypes,
+  definePreview
+} from '@storybook/react-vite';
 import { withDefaults } from 'commons/esm/.storybook/preview';
 
+// See issue: https://github.com/storybookjs/storybook/issues/33798
+const parameters: Preview['parameters'] = {
+  options: {
+    storySort: {
+      order: ['Configure your project', 'Readme']
+    }
+  }
+};
+
 export default {
+  parameters,
   ...withDefaults<ReactTypes>(
     (defaults) =>
       definePreview({
         ...defaults,
         parameters: {
           ...defaults.parameters,
-          options: {
-            storySort: {
-              order: ['Configure your project', 'Readme']
-            }
-          }
+          ...parameters
         }
       }) as any
   )
