7de1eec3dce6f703e04be6191b70f16549d24fff
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-02-23 22:50:03 -0800

    refactor

--- packages/html-ui/.storybook/preview.ts
+++ packages/html-ui/.storybook/preview.ts
@@ -1,24 +1,27 @@
 import 'react-syntax-highlighter';
 
 import {
-  type Preview,
   type WebComponentsTypes,
   definePreview
 } from '@storybook/web-components-vite';
 import { SyntaxHighlighter } from 'storybook/internal/components';
+import type { ProjectAnnotations } from 'storybook/internal/csf';
 import scss from 'react-syntax-highlighter/dist/esm/languages/prism/scss';
 import { withDefaults } from 'commons/esm/.storybook/preview';
 
 SyntaxHighlighter.registerLanguage('scss', scss);
 
 // See issue: https://github.com/storybookjs/storybook/issues/33798
-const parameters: Preview['parameters'] = {
+type Parameters = ProjectAnnotations<
+  WebComponentsTypes & { csf4: true }
+>['parameters'];
+const parameters: unknown = {
   options: {
     storySort: {
       order: ['Configure your project', 'Readme']
     }
   }
-};
+} as Parameters;
 
 export default {
   parameters,
@@ -27,7 +30,7 @@ export default {
       ...defaults,
       parameters: {
         ...defaults.parameters,
-        ...parameters,
+        ...(parameters as Parameters),
         docs: {
           source: {
             async transform(code) {
