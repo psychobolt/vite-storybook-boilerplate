89a72c14c47f00388879cd038e3e1d3253457d04
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-03-13 14:03:16 -0700

    refactor

--- packages/react-ui/.storybook/vitest.setup.ts
+++ packages/react-ui/.storybook/vitest.setup.ts
@@ -1,7 +1,7 @@
 import { beforeAll } from 'vitest';
 import { setProjectAnnotations } from '@storybook/react-vite';
 
-import preview from './preview';
+import preview from './preview.ts';
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
