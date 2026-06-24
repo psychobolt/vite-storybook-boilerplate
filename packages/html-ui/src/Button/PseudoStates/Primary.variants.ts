8d7c53d40c3eabc5d9daba4e097b7e2f3cb95219
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-06-24 14:44:17 -0700

    fix

--- packages/html-ui/src/Button/PseudoStates/Primary.variants.ts
+++ packages/html-ui/src/Button/PseudoStates/Primary.variants.ts
@@ -14,23 +14,23 @@ export type Args = Omit<Props, 'storyPseudo' | 'storyAttr'> &
   StoryPseudoStateArgs;
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories/introduction
-const meta = preview.type<{ args: Args }>().meta({
+const meta = preview.meta({
   ...primaryMeta.extend({
     // More on argTypes: https://storybook.js.org/docs/api/argtypes
     argTypes: getPseudoStateArgTypes(),
     decorators: [(Story) => html`<div style="padding: 3px">${Story()}</div>`]
   }),
-  title: 'Components/Button/Primary/Pseudo States',
-  args: {
-    storyPseudo: 'none',
-    storyAttr: 'none'
-  }
+  title: 'Components/Button/Primary/Pseudo States'
 });
 
 export default meta;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
-export const Primary = meta.story({
+export const Primary = meta.type<{ args: Args }>().story({
+  args: {
+    storyPseudo: 'none',
+    storyAttr: 'none'
+  },
   play({ canvasElement }) {
     const button = queryByRole(canvasElement, 'button');
     expect(button).toBeTruthy();
