4c7c68f3a92fee7985dbcc07d2c6dc66f29c07a8
psychobolt <560721+psychobolt@users.noreply.github.com>
2026-08-23 17:11:28 -0700

    fix: ignore PEM files in formatting

--- packages/commons/lint-staged.base.config.ts
+++ packages/commons/lint-staged.base.config.ts
@@ -1,14 +1,7 @@
 import type { Configuration } from 'lint-staged';
 
 const config: Configuration = {
-  '*': (filenames) => {
-    const formatFilenames = filenames.filter(
-      (filename) => !filename.endsWith('.pem')
-    );
-    return formatFilenames.length > 0
-      ? `yarn format -- ${formatFilenames.join(' ')}`
-      : [];
-  }
+  '*': (filenames) => `yarn format -- ${filenames.join(' ')}`
 };
 
 export default config;
